aed983fdbace789488930506a98b0af11f6f14bb
Vid Tadel <tadelv@gmail.com>
2026-07-27 14:34:32 +0200

    Revert "docs: clarify consumer-owned GATT-133 recovery"
    This reverts commit 15bc8c3cd7921247ad21b99afdb38d3ad621d27b.

--- android/src/test/kotlin/com/navideck/universal_ble/GattStatusErrorTest.kt
+++ android/src/test/kotlin/com/navideck/universal_ble/GattStatusErrorTest.kt
@@ -13,7 +13,6 @@ internal class GattStatusErrorTest {
         val error = createFlutterError(code, "Failed to write", status.toString())
 
         assertEquals(UniversalBleErrorCode.GATT_ERROR, code)
-        assertEquals(UniversalBleErrorCode.GATT_ERROR.raw.toString(), error.code)
         assertEquals("133", error.details)
     }
 
